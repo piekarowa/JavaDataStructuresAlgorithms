ee29b9be9f864aab82d98c8ef08667df4c8a7d27
piekarowa <pau.piekarska@gmail.com>
2023-05-22 12:24:50 +0200

    big O notation n^2

--- src/main/java/datastructures/BigON2.java
+++ src/main/java/datastructures/BigON2.java
@@ -1,18 +1,21 @@
 package datastructures;
 
-public class Main {
+public class BigON2 {
 
     public static void printItems(int n){
+
         for(int i = 0; i < n; i++){
-            System.out.println(i);
+            for(int j = 0; j < n; j++){
+                System.out.println(i + " " + j);
+            }
         }
-        for(int j = 0; j < n; j++){
-            System.out.println(j);
-        }
+
     }
 
     public static void main(String[] args) {
         printItems(10);
     }
- // n + n = 2n operations   O(2n) = O(n) dropping constances
+
+    // n * n = n^2 = O(n^2)
+
 }
