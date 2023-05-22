2d4c4d1ed52214cb106aa6e536efa3532e049a01
piekarowa <pau.piekarska@gmail.com>
2023-05-22 15:37:56 +0200

    classes LinkedList

---
+++ src/main/java/linkedList/LinkedList.java
@@ -0,0 +1,22 @@
+package linkedList;
+
+public class LinkedList {
+
+    public LinkedList(int value){}
+    //create ne Node
+
+
+    public void append(int value){}
+    //create new Node, add Node to end
+
+
+
+    public void prepend(int value){}
+    //create new Node, add Node to beginning
+
+//    public boolean insert(int index, int value){}
+    //create ne Node, insert going to create a new node and then insert the node at a particular index
+
+
+
+}
---
+++ src/main/java/pointers/Pointers.java
@@ -0,0 +1,8 @@
+package pointers;
+
+public class Pointers {
+
+    int num1 = 11;
+    int num2 = num1;
+
+}
