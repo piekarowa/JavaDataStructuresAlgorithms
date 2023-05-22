6e81b647cf87bc9b56469bd3d24e7434bce1f1f4
piekarowa <pau.piekarska@gmail.com>
2023-05-22 12:05:18 +0200

    big O notation

---
+++ src/main/java/datastructures/Main.java
@@ -0,0 +1,18 @@
+package datastructures;
+
+public class Main {
+
+    public static void printItems(int n){
+        for(int i = 0; i < n; i++){
+            System.out.println(i);
+        }
+        for(int j = 0; j < n; j++){
+            System.out.println(j);
+        }
+    }
+
+    public static void main(String[] args) {
+        printItems(10);
+    }
+ // n + n = 2n operations   O(2n) = O(n) dropping constances
+}
