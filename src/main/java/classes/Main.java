5841a898b8d93dee03797965ed70a843cc9f2840
piekarowa <pau.piekarska@gmail.com>
2023-05-22 13:20:46 +0200

    classes

---
+++ src/main/java/classes/Main.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cookie cookieOne = new Cookie("green");
+        Cookie cookieTwo = new Cookie("blue");
+
+    }
+}
