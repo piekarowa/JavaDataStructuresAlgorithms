7cedda44b938f42f5af07f00beae00ac6b86125e
piekarowa <pau.piekarska@gmail.com>
2023-05-22 13:27:33 +0200

    classes LinkedList

--- src/main/java/classes/LinkedList.java
+++ src/main/java/classes/LinkedList.java
@@ -1,4 +1,19 @@
 package classes;
 
+import org.w3c.dom.Node;
+
 public class LinkedList {
+
+    public LinkedList(int value){}
+
+    public void append(int value){}
+
+//    public Node removeLast(){}
+
+    public void prepend(int value){}
+
+//    public boolean insert(int index, int value){}
+
+//    public Node remove(int index){}
+
 }
