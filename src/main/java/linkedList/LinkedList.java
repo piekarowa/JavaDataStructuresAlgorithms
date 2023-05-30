36de8a420bf4a28c5a7f964dfd8562f59560f4ac
piekarowa <pau.piekarska@gmail.com>
2023-05-30 14:43:21 +0200

    removeLast - LinkedList

--- src/main/java/linkedList/LinkedList.java
+++ src/main/java/linkedList/LinkedList.java
@@ -74,4 +74,24 @@ length = 1;
         length = 0;
 
 }
+
+public Node removeLast(){
+        if(length == 0) return null;
+        Node temp = head;
+        Node pre = head;
+        while(temp.next != null){
+            pre = temp;
+            temp = temp.next;
+        }
+        tail = pre;
+        tail.next = null;
+        length--;
+        if(length == 0){
+            head = null;
+            tail = null;
+        }
+        return temp;
+
+}
+
 }
--- src/main/java/linkedList/Main.java
+++ src/main/java/linkedList/Main.java
@@ -7,8 +7,12 @@ public class Main {
         LinkedList myLinkedList = new LinkedList(1);
 
         myLinkedList.append(2);
-
-        myLinkedList.printList();
+        //(2) Items - Returns 2 Node
+        System.out.println(myLinkedList.removeLast().value);
+        //(1) Item - Returns 1 Node
+        System.out.println(myLinkedList.removeLast().value);
+        //(0) Items - Returns null
+        System.out.println(myLinkedList.removeLast());
 
     }
 
