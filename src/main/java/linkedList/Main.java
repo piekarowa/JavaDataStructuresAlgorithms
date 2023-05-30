91b04442349dae174ff22ac0793ce469352690ab
piekarowa <pau.piekarska@gmail.com>
2023-05-30 15:07:17 +0200

    prepend - LinkedList

--- src/main/java/linkedList/Main.java
+++ src/main/java/linkedList/Main.java
@@ -4,15 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LinkedList myLinkedList = new LinkedList(1);
+        LinkedList myLinkedList = new LinkedList(2);
 
-        myLinkedList.append(2);
-        //(2) Items - Returns 2 Node
-        System.out.println(myLinkedList.removeLast().value);
-        //(1) Item - Returns 1 Node
-        System.out.println(myLinkedList.removeLast().value);
-        //(0) Items - Returns null (without .value())
-        System.out.println(myLinkedList.removeLast());
+        myLinkedList.append(3);
+        myLinkedList.prepend(1);
+        myLinkedList.printList();
+
 
     }
 
